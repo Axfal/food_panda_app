063ecfa19c98ecfef09f0810ec0736902139dae2
Muhammad Anfal <anfalshah72@gmail.com>
2025-08-09 16:05:23 +0500

    small changes added

--- lib/config/components/custom_phone_field.dart
+++ lib/config/components/custom_phone_field.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:intl_phone_field/country_picker_dialog.dart';
 import 'package:intl_phone_field/intl_phone_field.dart';
 import 'package:excellent_trade_app/Utils/constants/app_colors.dart';
 
@@ -52,19 +53,49 @@ class CustomPhoneField extends StatelessWidget {
           borderRadius: BorderRadius.circular(12),
           borderSide: BorderSide.none,
         ),
+        enabledBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(12),
+          borderSide: BorderSide(color: AppColors.primary, width: 1),
+        ),
         focusedBorder: OutlineInputBorder(
           borderRadius: BorderRadius.circular(12),
           borderSide: BorderSide(color: AppColors.primary, width: 2),
-        ),
-      enabledBorder: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(12),
-          borderSide: BorderSide(color: AppColors.primary, width: 1),
         ),
       ),
       initialCountryCode: initialCountryCode,
       keyboardType: TextInputType.phone,
       dropdownIcon: Icon(Icons.arrow_drop_down, color: Colors.black),
       textInputAction: textInputAction,
+      dropdownTextStyle: TextStyle(color: Colors.grey.shade700),
+      pickerDialogStyle: PickerDialogStyle(
+        backgroundColor: Colors.white,
+        searchFieldInputDecoration: InputDecoration(
+          hintText: 'Search country',
+          hintStyle: TextStyle(color: Colors.grey.shade500, fontSize: 14),
+          prefixIcon: const Icon(Icons.search, color: Colors.black54),
+          filled: true,
+          fillColor: Colors.grey.shade100,
+          contentPadding: const EdgeInsets.symmetric(horizontal: 12, vertical: 8),
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(10),
+            borderSide: BorderSide(color: Colors.grey.shade300),
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(10),
+            borderSide: BorderSide(color: AppColors.primary, width: 1.5),
+          ),
+        ),
+        countryNameStyle: const TextStyle(
+          color: Colors.black,
+          fontSize: 16,
+          fontWeight: FontWeight.w500,
+        ),
+        countryCodeStyle: const TextStyle(
+          color: Colors.grey,
+          fontSize: 14,
+        ),
+      ),
+
       onChanged: (phone) => onChanged?.call(phone.completeNumber),
     );
   }
